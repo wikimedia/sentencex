b96f97e9a3c5b201b7ea9b566af72e2674a0053c
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-08-24 16:15:23 +0530

    Add more languages

--- sentencesegmenter/languages/__init__.py
+++ sentencesegmenter/languages/__init__.py
@@ -1,11 +1,13 @@
 from .ar import Arabic
 from .bg import Bulgarian
+from .bn import Bengali
 from .ca import Catalan
 from .da import Danish
 from .de import Deutsch
 from .el import Greek
 from .en import English
 from .fr import French
+from .gu import Gujarati
 from .hi import Hindi
 from .hy import Armenian
 from .it import Italian
@@ -15,14 +17,18 @@ from .ml import Malayalam
 from .mr import Marathi
 from .my import Burmese
 from .nl import Dutch
+from .or_ import Odia
+from .pa import Punjabi
 from .pl import Polish
 from .ru import Russian
 from .sk import Slovak
 from .ta import Tamil
+from .te import Telugu
 
 __all__ = [
     "Arabic",
     "Armenian",
+    "Bengali",
     "Bulgarian",
     "Burmese",
     "Catalan",
@@ -31,6 +37,7 @@ __all__ = [
     "Dutch",
     "English",
     "French",
+    "Gujarati",
     "Greek",
     "Hindi",
     "Italian",
@@ -38,9 +45,12 @@ __all__ = [
     "Kazakh",
     "Malayalam",
     "Marathi",
+    "Odia",
+    "Punjabi",
     "Polish",
     "Russian",
     "Slovak",
     "Spanish",
     "Tamil",
+    "Telugu",
 ]
