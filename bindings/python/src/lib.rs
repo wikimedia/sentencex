8cdc2f147c7ec053c49eb110b509036e9c0b0284
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-06-12 14:31:33 +0530

    refactor: simplify Python binding for sentence segmentation function

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,43 +1,9 @@
-use ::sentencex::languages::*;
 use pyo3::prelude::*;
+use sentencex::segment;
 
-#[pyclass]
-#[derive(Debug, Clone)]
-pub struct SentenceSegmenter {
-    language: LanguageOption,
-}
 
-#[derive(Debug, Clone)]
-#[pyclass]
-pub enum LanguageOption {
-    English,
-    Spanish,
-    Malayalam,
-    Portuguese,
-    Italian,
-}
-
+# Fix the below export of rust function to python. AI!
 #[pymethods]
-impl SentenceSegmenter {
-    #[new]
-    pub fn new(language: LanguageOption) -> Self {
-        SentenceSegmenter { language }
-    }
-
-    pub fn segment(&self, text: &str) -> Vec<String> {
-        match self.language {
-            LanguageOption::English => English {}.segment(text),
-            LanguageOption::Spanish => Spanish {}.segment(text),
-            LanguageOption::Malayalam => Malayalam {}.segment(text),
-            LanguageOption::Portuguese => Portuguese {}.segment(text),
-            LanguageOption::Italian => Italian {}.segment(text),
-        }
-    }
-}
-
-#[pymodule]
-fn sentencex(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<SentenceSegmenter>()?;
-    m.add_class::<LanguageOption>()?;
-    Ok(())
+pub fn segment(&self, language: &str, text: &str) -> Vec<String> {
+    segment(language, text)
 }
