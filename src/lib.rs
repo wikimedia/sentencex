b514a44313db8ac3a534153d5c343f736d262cb0
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-06-12 16:07:18 +0530

    refactor: simplify language_factory function by removing unnecessary condition

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ lazy_static::lazy_static! {
 fn language_factory(language_code: &str) -> Box<dyn Language> {
     let mut current_code = language_code;
     match current_code {
+        // move the blow mapping to a static hashmap. AI!
         "en" => return Box::new(English {}),
         "es" => return Box::new(Spanish {}),
         "ml" => return Box::new(Malayalam {}),
@@ -35,10 +36,7 @@ fn language_factory(language_code: &str) -> Box<dyn Language> {
             if let Some(fallbacks) = LANGUAGE_FALLBACKS.get(current_code) {
                 for next_code in fallbacks {
                     let instance = language_factory(next_code);
-                    // Fix the if condition below. AI!
-                    if instance {
-                        return instance;
-                    }
+                    return instance;
                 }
             }
             return Box::new(English {}); // Default to English
