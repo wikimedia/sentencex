c9d362a543e37dd7300eca7808d37191f5b915b5
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-10-22 16:35:17 +0530

    formatting

--- bindings/python/example.py
+++ bindings/python/example.py
@@ -1,10 +1,12 @@
 from sentencex import segment
 
-def main(): 
-    language_code = "en" 
-    text = "Hello world. This is a test." 
-    sentences = segment(language_code, text)
-    print("Segmented sentences:", sentences) 
 
-if __name__ == "__main__": 
+def main():
+    language_code = "en"
+    text = "Hello world. This is a test."
+    sentences = segment(language_code, text)
+    print("Segmented sentences:", sentences)
+
+
+if __name__ == "__main__":
     main()
