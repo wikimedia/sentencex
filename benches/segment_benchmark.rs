e5454ac4eed5325e4fa4bbe44266cb7c2c7c1376
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-10-23 12:31:00 +0530

    Remove warnings about deprecation

--- benches/segment_benchmark.rs
+++ benches/segment_benchmark.rs
@@ -1,5 +1,7 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use sentencex::segment; // Replace `sentencex` with the actual crate name if different
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use sentencex::segment;
 
 fn benchmark_segment(c: &mut Criterion) {
     let text = "This is a sentence. Here is another one. And yet another sentence follows.";
