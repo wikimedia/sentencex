d1b3564bd5a8154920c70fb70bcfb8fdffa67dc6
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-11-05 14:47:02 +0530

    Add abbreviations for Greek

--- src/languages/el.rs
+++ src/languages/el.rs
@@ -1,3 +1,5 @@
+use std::sync::LazyLock;
+
 use regex::Regex;
 
 use crate::constants::GLOBAL_SENTENCE_TERMINATORS;
@@ -7,9 +9,18 @@ use super::Language;
 #[derive(Debug, Clone)]
 pub struct Greek {}
 
+static GREEK_ABBREVIATIONS: LazyLock<Vec<String>> = LazyLock::new(|| {
+    include_str!("./abbrev/el.txt")
+        .lines()
+        .chain(include_str!("./abbrev/en.txt").lines())
+        .map(|line| line.trim().to_string())
+        .filter(|line| !line.starts_with("//") && !line.is_empty())
+        .collect()
+});
+
 impl Language for Greek {
     fn get_abbreviations(&self) -> &[String] {
-        &[]
+        &GREEK_ABBREVIATIONS
     }
 
     fn get_sentence_break_regex(&self) -> Regex {
