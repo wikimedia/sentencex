3fa5470f56d716c67d7569e1ecc4bb925aa4f942
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2026-03-06 14:16:37 +0530

    version bump

--- bindings/python/example.py
+++ bindings/python/example.py
@@ -1,4 +1,5 @@
 from sentencex import segment, get_sentence_boundaries
+import time
 
 
 def main():
@@ -10,6 +11,13 @@ def main():
         "en", "This is first sentence. This is another one."
     )
     print(boundaries)
+    with open("../../benchmarks/fixtures/shakespeare.txt") as bigfile:
+        text = bigfile.read()
+    t = time.time()
+    sentences = segment(language_code, text)
+    time_taken = time.time() - t
+    print("Speed : {:>20.2f} ms".format(time_taken * 1000))
+    print(f"Sentences: {len(sentences)}")
 
 
 if __name__ == "__main__":
